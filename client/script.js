8905809eaff6e2a63d12c634c269b833d15f4ade
naee44 <73422139+naee44@users.noreply.github.com>
2023-01-16 12:38:55 +0200

    update the server url

--- client/script.js
+++ client/script.js
@@ -88,7 +88,7 @@ const handleSubmit = async (e) => {
 
     // fetch the data from the server -> bot response
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://soul-la2r.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
